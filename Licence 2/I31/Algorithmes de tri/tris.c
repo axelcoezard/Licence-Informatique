2d18db0605af626c6ed526f933f0cf522eeea72c
Axel Coézard <axel.coezard@gmail.com>
2020-09-30 16:12:46 +0200

    Ajout de l'algo de selection

---
+++ Licence 2/I31/Algorithmes de tri/tris.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void tri_selection(int *tab, int n);
+void tri_insertion(int *tab, int n);
+void tri_fusion(int *tab, int n);
+
+int main()
+{
+    int tab[] = {1, 5, 8, 3, 9, 7};
+    tri_selection(tab, 6);
+    for (int i = 0; i < 6; i++)
+        printf("%d\n", tab[i]);
+}
+
+void tri_selection(int *tab, int n)
+{
+    int i, j, petit, i_petit;
+    int tmp;
+    for (i = 0; i < n - 1; i++)
+    {
+        i_petit = i;
+        petit = tab[i];
+        for (j = i; j < n; j++)
+        {
+            if (tab[j] < petit)
+            {
+                petit = tab[j];
+                i_petit = j;
+            }
+        }
+        tmp = tab[i_petit];
+        tab[i_petit] = tab[i];
+        tab[i] = tmp;
+    }
+}
