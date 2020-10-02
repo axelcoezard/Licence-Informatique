3d65c79e3ec0e2e299759f395c7f2702cdf9e72f
Axel Coézard <axel.coezard@gmail.com>
2020-10-02 14:44:40 +0200

    Fix

--- Licence 2/I31/TP 1/ex_2.c
+++ Licence 2/I31/TP 1/ex_2.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <math.h>
+
+#define PRINT(int) printf("%d\n", int)
 
 int main()
 {
-	// TROP LONG ET CHIANT A FAIRE.
-	// ON EST PAS EN INFO POUR FAIRE DU
-	// COPIER COLLER.
+	exo_12();
+	exo_14();
+}
+
+void exo_12()
+{
+	int x, y, z;
+	x = 2;
+	y = 1;
+	z = 0;
+	x = x && y || z;
+	PRINT(x);
+	PRINT(x * x || !y && z);
+	x = y = z;
+	z = x++ - 1;
+	PRINT(x);
+	PRINT(z);
+	z += -x++ + ++y;
+	PRINT(x);
+	PRINT(z);
+	z = x / ++x;
+	PRINT(z);
+}
+
+void exo_14()
+{
+	int x;
+	x = -3 + 4 * 5 - 6;
+	printf("%d\n", x);
+	x = 3 + 4 % 5 - 6;
+	printf("%d\n", x);
+	x = -3 * 4 % -6 / 5;
+	prinf("%d\n", x);
+	x = (7 + 6) % 5 / 2;
+	printf("%d\n", x);
 }
--- Licence 2/I31/TP 1/ex_7.c
+++ Licence 2/I31/TP 1/ex_7.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef union
+{
+	int entier;
+	float reel;
+} entreel;
+
 int main()
 {
+	entreel f;
+	printf("Entrer un réel:\n");
+	scanf("%f", &f.reel);
+	printf("%d", f.entier >> 15);
+	/*
+		TODO: Calculer la mantisse, le signe et l'exposant.
+	*/
+	printf("Le réeel [%f] est codé comme [%e][%f].");
 }
