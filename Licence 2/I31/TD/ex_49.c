815d8572bdcdad27ca1ffe7498312be635e39a36
Axel Coézard <axel.coezard@gmail.com>
2020-10-02 11:39:31 +0200

    Ajout de l'exo 49 du TD I31

--- Licence 2/I31/TD/ex_49.c
+++ Licence 2/I31/TD/ex_49.c
@@ -3,15 +3,29 @@
 
 int main()
 {
-	int a, b, c;
+	double a, b, c;
 
-	printf("Choisir 3 entiers:\n");
-	scanf("%d", &a);
-	scanf("%d", &b);
-	scanf("%d", &c);
+	printf("Choisir une équation aX^2 + bX + c = 0:\n");
+	printf("a = ");
+	scanf("%lf", &a);
+	printf("b = ");
+	scanf("%lf", &b);
+	printf("c = ");
+	scanf("%lf", &c);
 
-	int minimum = a > b ? b : a;
-	minimum = minimum > c ? c : minimum;
+	if (a = 0)
+		if (b != 0)
+			printf("La solution est: x = %lf\n", -c / b);
+		else
+			printf("Aucune solution.\n");
 
-	printf("Minimum = %d\n", minimum);
+	double delta = pow(b, 2) - 4 * a * c;
+	printf("Delta = %lf\n", delta);
+
+	if (delta < 0)
+		printf("Aucune solution réelle.\n");
+	else if (delta == 0)
+		printf("Une seule solution: x = %lf \n", -b / 2 / a);
+	else if (delta > 0)
+		printf("Deux solutions:\n- x1 = %lf\n- x2 = %lf\n", (-b - sqrt(delta)) / a / 2, (-b + sqrt(delta)) / a / 2);
 }
