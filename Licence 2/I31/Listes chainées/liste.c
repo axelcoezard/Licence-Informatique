fefe573374e777199f07961a81c0a370c342c71c
Axel Coézard <axel.coezard@gmail.com>
2020-10-07 16:50:52 +0200

    Ajout exposé Anglais, Matrices & Listes chainées

--- Licence 2/I31/Listes chainées/liste.c
+++ Licence 2/I31/Listes chainées/liste.c
@@ -7,7 +7,7 @@ typedef struct
 } Maillon;
 
 int liste_vide(Maillon *tete);
-void add_liste(Maillon *tete, int valeur);
+void add_liste(Maillon **tete, int valeur);
 
 int main()
 {
@@ -18,11 +18,11 @@ int liste_vide(Maillon *tete)
 	return (tete == NULL);
 }
 
-void add_liste(Maillon *tete, int valeur)
+void add_liste(Maillon **tete, int valeur)
 {
 	Maillon *nouveau;
 	nouveau = (Maillon *)malloc(sizeof(Maillon));
 	nouveau->valeur = valeur;
-	nouveau->suivant = tete;
-	tete = nouveau;
+	nouveau->suivant = *tete;
+	*tete = nouveau;
 }
