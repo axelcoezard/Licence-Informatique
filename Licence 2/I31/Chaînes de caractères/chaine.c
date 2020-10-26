2b21d751cc3ba6e3a044bebd9aef79706c46e75d
Axel Coézard <axel.coezard@gmail.com>
2020-10-26 11:18:03 +0100

    Ajout de cherche_str() pour I31

--- Licence 2/I31/Chaînes de caractères/chaine.c
+++ Licence 2/I31/Chaînes de caractères/chaine.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void modif(char *chaine);
 
@@ -15,4 +16,19 @@ void modif(char *chaine)
 			chaine[i] = 'E';
 		i += 1;
 	}
+}
+
+int cherche_str(char *c, char *k)
+{
+	int ic, ik;
+	int lc = len(c), lk = len(k);
+	for (ic = 0; ic < lc - lk; ic++)
+	{
+		ik = 0;
+		while (k[ik] != '\0' && k[ik] == c[ic + ik])
+			ik += 1;
+		if (k[ik] == '\0')
+			return ic;
+	}
+	return -1;
 }
