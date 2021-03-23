7fd58f17722277ee3b9fdabdace95530a179ddbc
Axel Coézard <axel.coezard@gmail.com>
2021-03-23 09:14:58 +0100

    Ajout de l'ex_1 du TP5 I41

---
+++ Licence 2/I41/TP 5/ex_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ex_1.h";
+
+int main() {
+    ullong R = Factorielle(20);
+    printf("%lld\n", R);
+}
+
+/**
+ * \brief Calcule n factorielle
+ * \param n Un entier non-signé
+ * \return 1 * 2 * ... * n
+ */
+ullong Factorielle(uchar n) {
+    ullong i = 1;
+    while (n > 0) {
+        i *= n;
+        n--;
+    }
+    return i;
+}
---
+++ Licence 2/I41/TP 5/ex_1.h
@@ -0,0 +1,4 @@
+typedef unsigned char uchar;
+typedef unsigned long long int ullong;
+
+ullong Factorielle(uchar n);
