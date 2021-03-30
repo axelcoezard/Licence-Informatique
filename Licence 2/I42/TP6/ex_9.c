cffdcb6a4306e6b05c449fc481bc34c85dbd32b5
Axel Coézard <axel.coezard@gmail.com>
2021-03-30 21:53:38 +0200

    fix

--- Licence 2/I42/TP6/ex_9.c
+++ Licence 2/I42/TP6/ex_9.c
@@ -4,31 +4,34 @@
 #include <signal.h>
 #include <time.h>
 
-uint* generate_tab(uint n);
+uint *generate_tab(uint n);
 
-int main() {
+int main()
+{
     int n = 6;
 
     // Génère deux vecteurs v1 et v2 aléatoires.
-    uint* V1 = generate_tab(n);
-    uint* V2 = generate_tab(n);
+    uint *V1 = generate_tab(n);
+    uint *V2 = generate_tab(n);
 
     // Somme des travaux des processus
     int somme = 0;
 
     int i = 0;
-    while (i < n) {
+    while (i < n)
+    {
         // Créé un tube
         int tube[2];
         pipe(tube);
         // Créé un processus enfant
-        if(fork() == 0) {
+        if (fork() == 0)
+        {
             int n_pid = getpid();
             // Calcule v1[i] * v2[i].
             uint value = V1[i] * V2[i];
             // Converti le value en chaine de caractère.
             char message[10] = "";
-            sprintf(message, "%d", value);z
+            sprintf(message, "%d", value);
             // Ecriture dans la value dans le pipe
             write(tube[1], message, 10);
             close(tube[0]);
@@ -54,15 +57,17 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-uint* generate_tab(uint n) {
-    uint* tab = malloc(n * sizeof(uint));
+uint *generate_tab(uint n)
+{
+    uint *tab = malloc(n * sizeof(uint));
     uint i = 0;
     struct timespec spec;
 
     clock_gettime(0, &spec);
     srand(spec.tv_nsec);
     printf("Vecteur: ");
-    while (i < n) {
+    while (i < n)
+    {
         tab[i] = rand() % 10;
         printf("%d ", tab[i]);
         i++;
