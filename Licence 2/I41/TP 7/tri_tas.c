991cf09afaab7ac5ad8558c3441466d2c7bbe7a7
Axel Coézard <axel.coezard@gmail.com>
2021-04-06 13:42:44 +0200

    Ajout de Entasser(...), TrisTas(..), generate_perms(...)

--- Licence 2/I41/TP 7/tri_tas.c
+++ Licence 2/I41/TP 7/tri_tas.c
@@ -16,7 +16,7 @@ void Echanger(int *T, uint a, uint b)
 
 void Tamiser(int *T, uint i, uint n)
 {
-    uint child;
+    uint child = 2 * i;
     if (child < n && T[child + 1] > T[child])
         child++;
     if (child <= n && T[i] < T[child])
@@ -24,4 +24,49 @@ void Tamiser(int *T, uint i, uint n)
         Echanger(T, i, child);
         Tamiser(T, child, n);
     }
+}
+
+void Entasser(int *T, uint n)
+{
+    uint i = n / 2;
+    while (i > 0)
+    {
+        Tamiser(T, i, n);
+        i--;
+    }
+}
+
+void TriTas(int *T, uint n)
+{
+    Entasser(T, n);
+    uint k = n;
+    while (k > 0)
+    {
+        Echanger(T, 1, k);
+        Tamiser(T, 1, k - 1);
+        k--;
+    }
+}
+
+/**
+ * \brief Génère une tableau contenant n entiers permutés.
+ * \param n La taille du tableau et la valeur maximal de chaque entier.
+ * \return un tableau d'entiers permutés.
+ */
+int *generate_perms(int n)
+{
+    int *perms = malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++)
+        perms[i] = i + 1;
+
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        int index = rand() % n;
+        int tmp = perms[i];
+        perms[i] = perms[index];
+        perms[index] = tmp;
+    }
+
+    return perms;
 }
--- Licence 2/I41/TP 7/tri_tas.h
+++ Licence 2/I41/TP 7/tri_tas.h
@@ -5,3 +5,7 @@ void Echanger(int *T, uint a, uint b);
 void Tamiser(int *T, uint i, uint n);
 
 void Entasser(int *T, uint n);
+
+void TriTas(int *T, uint n);
+
+int *generate_perms(int n);
