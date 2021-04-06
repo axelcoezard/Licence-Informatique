e214964afc4a8fd15580a5b40aeebf3217d3e99c
Axel Coézard <axel.coezard@gmail.com>
2021-04-06 15:00:32 +0200

    Ajout de gnuplot(...)

--- Licence 2/I41/TP 7/tri_tas.c
+++ Licence 2/I41/TP 7/tri_tas.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 #include "tri_tas.h"
 
 int main(int argc, char **argv)
 {
+    // Récupère dans argv la taille n du tableau.
+    int n = atoi(argv[1]);
+    // Récupère dans argv la durée requise pour l'évaluation.
+    int length = atoi(argv[2]);
+    //
+    int *eval_tri = malloc(length * sizeof(int));
+    int *eval_log = malloc(length * sizeof(int));
+    //
+    FILE *file_1 = fopen("./graph_eval.txt", "w+");
+    FILE *file_2 = fopen("./graph_log.txt", "w+");
+    //
+    if (file_1 != NULL && file_2 != NULL)
+    {
+        int i = 0;
+        while (i < length)
+        {
+            // Génère une tableau de taille n, de permutations aléatoires.
+            int *perms = generate_perms(i);
+            int coeff = 7;
+            int eval_tri = TriTas(perms, i) / coeff;
+            // Ecriture dans le fichier
+            fprintf(file_1, "%d %d\n", i, eval_tri);
+            fprintf(file_2, "%d %f\n", i, i * log10(i));
+            i++;
+        }
+        fclose(file_1);
+        fclose(file_2);
+    }
+
+    system("gnuplot -persist -e \"plot './graph_eval.txt' with lines linewidth 1.5, './graph_log.txt' with lines linewidth 1.5\"");
 }
 
 void Echanger(int *T, uint a, uint b)
@@ -14,38 +46,43 @@ void Echanger(int *T, uint a, uint b)
     T[b] = tmp;
 }
 
-void Tamiser(int *T, uint i, uint n)
+int Tamiser(int *T, uint i, uint n)
 {
+    int C = 2;
     uint child = 2 * i;
     if (child < n && T[child + 1] > T[child])
         child++;
     if (child <= n && T[i] < T[child])
     {
         Echanger(T, i, child);
-        Tamiser(T, child, n);
+        C += Tamiser(T, child, n);
     }
+    return C;
 }
 
-void Entasser(int *T, uint n)
+int Entasser(int *T, uint n)
 {
     uint i = n / 2;
+    int C = i;
     while (i > 0)
     {
-        Tamiser(T, i, n);
+        C += Tamiser(T, i, n);
         i--;
     }
+    return C;
 }
 
-void TriTas(int *T, uint n)
+int TriTas(int *T, uint n)
 {
-    Entasser(T, n);
     uint k = n;
+    int C = Entasser(T, n) + k;
     while (k > 0)
     {
         Echanger(T, 1, k);
-        Tamiser(T, 1, k - 1);
+        C += Tamiser(T, 1, k - 1);
         k--;
     }
+    return C;
 }
 
 /**
@@ -59,7 +96,9 @@ int *generate_perms(int n)
     for (int i = 0; i < n; i++)
         perms[i] = i + 1;
 
-    srand(time(NULL));
+    struct timespec spec;
+    clock_gettime(0, &spec);
+    srand(spec.tv_nsec);
     for (int i = 0; i < n; i++)
     {
         int index = rand() % n;
--- Licence 2/I41/TP 7/tri_tas.h
+++ Licence 2/I41/TP 7/tri_tas.h
@@ -2,10 +2,10 @@ typedef unsigned int uint;
 
 void Echanger(int *T, uint a, uint b);
 
-void Tamiser(int *T, uint i, uint n);
+int Tamiser(int *T, uint i, uint n);
 
-void Entasser(int *T, uint n);
+int Entasser(int *T, uint n);
 
-void TriTas(int *T, uint n);
+int TriTas(int *T, uint n);
 
 int *generate_perms(int n);
