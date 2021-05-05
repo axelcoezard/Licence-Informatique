f2c7c05f49cb2b2909565aab93a5a32852265580
Axel Coézard <axel.coezard@gmail.com>
2021-05-06 01:17:49 +0200

    Rework I42: Exo 4 Tp5

--- Licence 2/I42/TP5/ex_4.c
+++ Licence 2/I42/TP5/ex_4.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
 
-int fils_pid;
-void affiche_fils_pid();
+int child_pid;
+void handleSignal();
 
-int main() {
-    int pere_pid = getpid();
-    if((fils_pid = fork()) == 0) {
-        printf("J'envois un signal SIGUSR1 à mon père:\n");
-        sleep(5);   
-        kill(pere_pid, SIGUSR1);
-    } else {
-        signal(SIGUSR1, affiche_fils_pid);
+int main()
+{
+    int pid = getpid();
+    if ((child_pid = fork()) == 0)
+    {
+        printf("J'envois un signal SIGUSR1 à mon père...\n");
+        sleep(5);
+        kill(pid, SIGUSR1);
+        exit(0);
     }
-    while(1);
+
+    signal(SIGUSR1, handleSignal);
+    pause();
+    exit(0);
 }
 
-void affiche_fils_pid() {
-    printf("Je suis le père, mon fils a pour PID: %i\n", fils_pid);
+void handleSignal()
+{
+    printf("Je suis le père, mon fils a pour PID: %i\n", child_pid);
 }
-
