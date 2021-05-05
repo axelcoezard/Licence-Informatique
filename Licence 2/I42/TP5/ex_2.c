19399c7dda8aaeb4e3dfae6531a6af70bcbf9970
Axel Coézard <axel.coezard@gmail.com>
2021-05-06 01:14:42 +0200

    Rework I42: Exo 2 Tp 5

--- Licence 2/I42/TP5/ex_2.c
+++ Licence 2/I42/TP5/ex_2.c
@@ -3,14 +3,21 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main() {
+/**
+ *  
+ */
+int main()
+{
     int p_pid = getpid();
-    printf("Le pid est: %i\n", p_pid);
-
     int c_pid;
-    if ((c_pid = fork()) == 0) {
+
+    p_pid = getpid();
+    if ((c_pid = fork()) == 0)
+    {
         printf("Je suis le fils, mon père a pour PID:  %i\n", p_pid);
-    } else {
-        printf("Je suis le père, mon fils a pour PID:  %i\n", c_pid);
+        exit(0);
     }
+    printf("Le PID du père est: %i\n", p_pid);
+    printf("Je suis le père, mon fils a pour PID:  %i\n", c_pid);
+    exit(0);
 }
